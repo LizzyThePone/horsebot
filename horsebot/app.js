b69680f7f06b907e20b0c5589b478bfc56e12ec5
Elizabeth <lizzymcgehee99@gmail.com>
2018-10-18 13:03:29 -0500

    Split commands and add config/default config loader

--- horsebot/app.js
+++ horsebot/app.js
@@ -3,14 +3,34 @@
 const _D = require('discord.js');
 const _c = new _D.Client();
 const chalk = require('chalk');
-const commandMap = new Map();
-var queue = new Map();
-var prefix = ']';
-var embedColor = 0x00a1ff;
-var errorColor = 0xff0000;
-var lineColor = 0xb942f4;
-var readyColor = 0x41f489;
-var owner;
+const fs = require('fs-extra');
+var config = {};
+
+fs.exists('./config.json').then(exists => {
+    if (!exists) {
+        fs.ensureFile('./config.json').then(() => {
+            console.log('Created config.json')
+            fs.readJson('./default_config.json').then(defaultConfig => {
+                fs.writeJson('./config.json', defaultConfig).then(() => {
+                    console.log('Wrote default_config.json to config.json')
+                    console.log('Add your token into that file now')
+                    fs.readJson('./config.json').then(configFile => {
+                        config = configFile
+                    })
+                })
+            })
+        })
+    } else {
+        fs.readJson('./config.json').then(configFile => {
+            console.log('Config loaded!')
+            config = configFile
+            _c.login(config.token)
+        })
+    }
+})
+
+config.owner;
+const commandMap = require('./commands')(_D, _c, config);
 
 var logCommand = m => {
     var denied
@@ -19,150 +39,33 @@ var logCommand = m => {
     } else {
         denied = "ALLOWED"
     }
-    console.log(chalk.hex(lineColor)('-----------------------------'))
-    console.log(chalk.red(`${denied}\nCommand: ${chalk.blue(m.content.toLocaleLowerCase().slice(prefix.length))}\nUser: ${chalk.blue(m.author.tag)}\nChannel: ${chalk.blue(m.channel.name || '[DM]')}\nTime: ${chalk.blue(new Date().toString())}`));
-    console.log(chalk.hex(lineColor)('-----------------------------\n'))
-}
-
-commandMap['ping'] = {
-    func: m => {
-        const pong = _c.ping
-        const e = new _D.MessageEmbed()
-            .setColor(embedColor)
-            .setDescription(`Pong! ${pong}ms`)
-            .setFooter('Average of last 3 pings')
-        m.channel.send(e)
-    },
-    check: m => {
-        return true
-    },
-}
-
-commandMap['eval'] = {
-    func: m => {
-        try {
-            var evalStr = eval(m.content.replace(prefix + "eval ", ""));
-            var e = new _D.MessageEmbed()
-                .setTitle("Result:")
-                .setDescription(evalStr)
-                .setColor(embedColor);
-            m.channel.send(e);
-        } catch (e) {
-            var e = new _D.MessageEmbed()
-                .setTitle("Error:")
-                .setDescription(e.stack)
-                .setColor(errorColor);
-            m.channel.send(e);
-        }
-    },
-    check: m => {
-        if (m.author.id !== owner.id) {
-            var e = new _D.MessageEmbed()
-                .setTitle("Unable to run:")
-                .setDescription('That command is restricted to the bot owner!')
-                .setColor(errorColor);
-            m.channel.send(e);
-            return false
-        } else {
-            return true
-        }
-    }
-}
-
-commandMap["prefix"] = {
-    func: m => {
-        prefix = m.content.replace(prefix + "prefix ", "");
-        var e = new _D.MessageEmbed()
-            .setTitle(`\u2705 Prefix changed to ${prefix}`)
-            .setColor(embedColor);
-        m.channel.send(e);
-    },
-    check: m => {
-        if (m.author.id !== owner.id) {
-            var e = new _D.MessageEmbed()
-                .setTitle("Unable to run:")
-                .setDescription('That command is restricted to the bot owner!')
-                .setColor(errorColor);
-            m.channel.send(e);
-            return false
-        } else {
-            return true
-        }
-    }
-}
-
-commandMap['prune'] = {
-    func: m => {
-        var deleteAmmount = parseInt(m.content.replace(prefix + "prune ", "")) || 100;
-        if (deleteAmmount > 100 || deleteAmmount < 2) {
-            var e = new _D.MessageEmbed()
-                .setDescription('Number must be between 2 and 100')
-                .setColor(errorColor);
-            m.channel.send(e);
-            return
-        }
-        m.channel.bulkDelete(deleteAmmount).then(messages => {
-            deleteAmmount = messages.array().length
-            var e = new _D.MessageEmbed()
-                .setTitle(`\u2705 Deleted ${deleteAmmount} messages!`)
-                .setColor(embedColor);
-            m.channel.send(e).then(response => {
-                response.delete(3000);
-            });
-        }) 
-    },
-    check: m => {
-        if (m.channel.type !== "text") {
-            var e = new _D.MessageEmbed()
-                .setTitle("Unable to run:")
-                .setDescription('This can only run in a server!')
-                .setColor(errorColor);
-            m.channel.send(e);
-            return false
-        } else if (!m.channel.permissionsFor(m.member).has('MANAGE_MESSAGES')) {
-            var e = new _D.MessageEmbed()
-                .setTitle("Unable to run:")
-                .setDescription('You must be able to delete messages here!')
-                .setColor(errorColor);
-            m.channel.send(e);
-            return false
-        } else if (!m.channel.permissionsFor(m.guild.me).has('MANAGE_MESSAGES')) {
-            var e = new _D.MessageEmbed()
-                .setTitle("Unable to run:")
-                .setDescription('I don\'t have permission to do that here!')
-                .setColor(errorColor);
-            m.channel.send(e);
-            return false
-        } else {
-            return true
-        }
-    }
+    console.log(chalk.hex(config.lineColor)('-----------------------------'))
+    console.log(chalk.red(`${denied}\nCommand: ${chalk.blue(m.content.toLocaleLowerCase().slice(config.prefix.length))}\nUser: ${chalk.blue(m.author.tag)}\nChannel: ${chalk.blue(m.channel.name || '[DM]')}\nTime: ${chalk.blue(new Date().toString())}`));
+    console.log(chalk.hex(config.lineColor)('-----------------------------\n'))
 }
 
 _c.on('ready', () => {
-    console.log(chalk.hex(lineColor)('-----------------------------'))
-    console.log(chalk.hex(readyColor)(`Bot started as ${chalk.blue(_c.user.tag)}`));
+    console.log(chalk.hex(config.lineColor)('-----------------------------'))
+    console.log(chalk.hex(config.readyColor)(`Bot started as ${chalk.blue(_c.user.tag)}`));
     _c.fetchApplication().then(app => {
-        owner = app.owner;
-        console.log(chalk.hex(readyColor)(`Owner set to ${chalk.blue(owner.tag)}`));
-        console.log(chalk.hex(lineColor)('-----------------------------\n'))
+        config.owner = app.owner;
+        console.log(chalk.hex(config.readyColor)(`Owner set to ${chalk.blue(config.owner.tag)}`));
+        console.log(chalk.hex(config.lineColor)('-----------------------------\n'))
     });
 });
 
 _c.on('message', m => {
-    if (!m.content.startsWith(prefix)) return 
-    if (commandMap[m.content.toLocaleLowerCase().split(' ')[0].slice(prefix.length)].check) {
-        if (commandMap[m.content.toLocaleLowerCase().split(' ')[0].slice(prefix.length)].check(m) !== true) {
+    if (!m.content.startsWith(config.prefix)) return 
+    if (commandMap[m.content.toLocaleLowerCase().split(' ')[0].slice(config.prefix.length)].check) {
+        if (commandMap[m.content.toLocaleLowerCase().split(' ')[0].slice(config.prefix.length)].check(m) !== true) {
             m.denied = true
             logCommand(m)
             return
         }
     }
-    if (commandMap[m.content.toLocaleLowerCase().split(' ')[0].slice(prefix.length)]) {
-        commandMap[m.content.toLocaleLowerCase().split(' ')[0].slice(prefix.length)].func(m);
+    if (commandMap[m.content.toLocaleLowerCase().split(' ')[0].slice(config.prefix.length)]) {
+        commandMap[m.content.toLocaleLowerCase().split(' ')[0].slice(config.prefix.length)].func(m);
         m.denied = false
         logCommand(m)
     }
 });
-
-_c.login('');
---
+++ horsebot/commands.js
@@ -0,0 +1,121 @@
+﻿module.exports = (_D, _c, fucks) => {
+
+    var commandMap = new Map()
+
+    commandMap['ping'] = {
+        func: m => {
+            const pong = _c.ping
+            const e = new _D.MessageEmbed()
+                .setColor(fucks.embedColor)
+                .setDescription(`Pong! ${pong}ms`)
+                .setFooter('Average of last 3 pings')
+            m.channel.send(e)
+        },
+        check: m => {
+            return true
+        },
+    }
+
+    commandMap['eval'] = {
+        func: m => {
+            try {
+                var evalStr = eval(m.content.replace(fucks.prefix + "eval ", ""));
+                var e = new _D.MessageEmbed()
+                    .setTitle("Result:")
+                    .setDescription(evalStr)
+                    .setColor(fucks.embedColor);
+                m.channel.send(e);
+            } catch (e) {
+                var e = new _D.MessageEmbed()
+                    .setTitle("Error:")
+                    .setDescription(e.stack)
+                    .setColor(fucks.errorColor);
+                m.channel.send(e);
+            }
+        },
+        check: m => {
+            if (m.author.id !== fucks.owner.id) {
+                var e = new _D.MessageEmbed()
+                    .setTitle("Unable to run:")
+                    .setDescription('That command is restricted to the bot owner!')
+                    .setColor(fucks.errorColor);
+                m.channel.send(e);
+                return false
+            } else {
+                return true
+            }
+        }
+    }
+
+    commandMap["prefix"] = {
+        func: m => {
+            fucks.prefix = m.content.replace(fucks.prefix + "prefix ", "");
+            var e = new _D.MessageEmbed()
+                .setTitle(`\u2705 Prefix changed to ${fucks.prefix}`)
+                .setColor(fucks.embedColor);
+            m.channel.send(e);
+        },
+        check: m => {
+            if (m.author.id !== fucks.owner.id) {
+                var e = new _D.MessageEmbed()
+                    .setTitle("Unable to run:")
+                    .setDescription('That command is restricted to the bot owner!')
+                    .setColor(fucks.errorColor);
+                m.channel.send(e);
+                return false
+            } else {
+                return true
+            }
+        }
+    }
+
+    commandMap['prune'] = {
+        func: m => {
+            var deleteAmmount = parseInt(m.content.replace(fucks.prefix + "prune ", "")) || 100;
+            if (deleteAmmount > 100 || deleteAmmount < 2) {
+                var e = new _D.MessageEmbed()
+                    .setDescription('Number must be between 2 and 100')
+                    .setColor(fucks.errorColor);
+                m.channel.send(e);
+                return
+            }
+            m.channel.bulkDelete(deleteAmmount).then(messages => {
+                deleteAmmount = messages.array().length
+                var e = new _D.MessageEmbed()
+                    .setTitle(`\u2705 Deleted ${deleteAmmount} messages!`)
+                    .setColor(fucks.embedColor);
+                m.channel.send(e).then(response => {
+                    response.delete(3000);
+                });
+            })
+        },
+        check: m => {
+            if (m.channel.type !== "text") {
+                var e = new _D.MessageEmbed()
+                    .setTitle("Unable to run:")
+                    .setDescription('This can only run in a server!')
+                    .setColor(fucks.errorColor);
+                m.channel.send(e);
+                return false
+            } else if (!m.channel.permissionsFor(m.member).has('MANAGE_MESSAGES')) {
+                var e = new _D.MessageEmbed()
+                    .setTitle("Unable to run:")
+                    .setDescription('You must be able to delete messages here!')
+                    .setColor(fucks.errorColor);
+                m.channel.send(e);
+                return false
+            } else if (!m.channel.permissionsFor(m.guild.me).has('MANAGE_MESSAGES')) {
+                var e = new _D.MessageEmbed()
+                    .setTitle("Unable to run:")
+                    .setDescription('I don\'t have permission to do that here!')
+                    .setColor(fucks.errorColor);
+                m.channel.send(e);
+                return false
+            } else {
+                return true
+            }
+        }
+    }
+
+    return commandMap
+}
